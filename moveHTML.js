10b6fb986adea9eb4581a6ea6c41a0e9826f5f04
nato-git <s.f.h.o1219@gmail.com>
2025-10-30 09:28:49 +0900

    漢字辞典の追加

--- moveHTML.js
+++ moveHTML.js
@@ -1,20 +1,40 @@
 const caption = {
-  title: ['ドラクエ１竜王戦', '2DShootingGame'],
+  title: ['ドラクエ１竜王戦', 'Juppyter', '小学生向け漢字辞典'],
   explanation: [
+    //ryuou_battle
     `GoogleColabで制作したFC版ドラゴンクエスト１竜王戦の再現です。<br>
     Ipython.displayを利用し、Python内でHTML,Javascriptを呼び出しています。<br>`,
+    //Juppyter
     `2年文化祭でグループ制作した2Dのシューティングゲームです。<br>
     Unity,VScode,Githubで制作しました。<br>
     敵を倒すとアルファベットをドロップし、特定のアルファベットを集めることで技を発動することができます。`,
+    //kanji_jiten
+    `個人で制作した小学生向けの漢字辞典です。<br>
+    文部科学省が出しているcsvファイルを利用して1年生から6年生までの漢字を表示させています。<br>
+    また、tesseractを利用して、画像から小学生で習う漢字のみを抽出して調べることもできます。`,
   ],
-  picture1: ['project_img/ryuou_battle_1.png', 'project_img/2dshoot_1.png'],
-  picture2: ['project_img/ryuou_battle_2.png', 'project_img/2dshoot_1.png'],
-  picture3: ['project_img/ryuou_battle_3.png', 'project_img/2dshoot_1.png'],
+  picture1: [
+    'project_img/ryuou_battle_1.png',
+    'project_img/2dshoot_1.png',
+    'project_img/kanji_1.png',
+  ],
+  picture2: [
+    'project_img/ryuou_battle_2.png',
+    'project_img/2dshoot_1.png',
+    'project_img/kanji_2.png',
+  ],
+  picture3: [
+    'project_img/ryuou_battle_3.png',
+    'project_img/2dshoot_1.png',
+    'project_img/kanji_3.png',
+  ],
   url: [
     //ryuou_battle
     'https://colab.research.google.com/drive/1vlJxniejNXWUlFSLyqM3Qte1vUIexAkA?usp=sharing',
     //2dshooting
     'https://github.com/panko200/2D_Shooting_Game_Team',
+    //kanji_jiten
+    'https://nato-git.github.io/kanji-dictionary/home',
   ],
 };
 
