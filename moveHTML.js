38775706b198edf7893186ee69270e033b6b73c9
nato-git <s.f.h.o1219@gmail.com>
2025-12-08 13:32:41 +0900

    CRLの追加

--- moveHTML.js
+++ moveHTML.js
@@ -1,5 +1,5 @@
 const caption = {
-  title: ['ドラクエ１竜王戦', 'Juppyter', '小学生向け漢字辞典'],
+  title: ['ドラクエ１竜王戦', 'Juppyter', '小学生向け漢字辞典', 'CRL'],
   explanation: [
     //ryuou_battle
     `GoogleColabで制作したFC版ドラゴンクエスト１竜王戦の再現です。<br>
@@ -12,29 +12,37 @@ const caption = {
     `個人で制作した小学生向けの漢字辞典です。<br>
     文部科学省が出しているcsvファイルを利用して1年生から6年生までの漢字を表示させています。<br>
     また、tesseractを利用して、画像から小学生で習う漢字のみを抽出して調べることもできます。`,
+    //CRL
+    `個人で制作したAndroidアプリです。<br>
+    AndroidStudio,Kotlinで制作しました。<ruby>CRL<rt>シール</rt></ruby>(ChaRacterListapp)は、作品のキャラなどを作品ごとにまとめることができます。<br>`,
   ],
   picture1: [
     'project_img/ryuou_battle_1.png',
     'project_img/2dshoot_1.png',
     'project_img/kanji_1.png',
+    'project_img/crl_1.png',
   ],
   picture2: [
     'project_img/ryuou_battle_2.png',
     'project_img/2dshoot_2.png',
     'project_img/kanji_2.png',
+    'project_img/crl_2.png',
   ],
   picture3: [
     'project_img/ryuou_battle_3.png',
     'project_img/2dshoot_3.png',
     'project_img/kanji_3.png',
+    'project_img/crl_3.png',
   ],
   url: [
     //ryuou_battle
     'https://colab.research.google.com/drive/1vlJxniejNXWUlFSLyqM3Qte1vUIexAkA?usp=sharing',
     //2dshooting
-    'https://github.com/panko200/2D_Shooting_Game_Team',
+    'https://github.com/panko200/2D_Shooting_Game_Team.git',
     //kanji_jiten
     'https://nato-git.github.io/kanji-dictionary/home',
+    //CRL
+    'https://github.com/nato-git/CharacterListApp.git',
   ],
 };
 
