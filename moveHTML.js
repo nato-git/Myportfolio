9bee27b4a38d39ef9d85d7ae9b9024414dfd1729
nato-git <s.f.h.o1219@gmail.com>
2026-01-03 02:30:55 +0900

    fillsupporterと家に帰りたい。の追加

--- moveHTML.js
+++ moveHTML.js
@@ -15,24 +15,36 @@ const caption = {
     //CRL
     `個人で制作したAndroidアプリです。<br>
     AndroidStudio,Kotlinで制作しました。<ruby>CRL<rt>シール</rt></ruby>(ChaRacterListapp)は、作品のキャラなどを作品ごとにまとめることができます。<br>`,
+    //FillSupporter
+    `個人で制作したマインクラフト統合版用アドオンです。<br>
+    Fillコマンドを手軽に実行することができるアドオンです。Javascriptで制作しました。`,
+    //Gohome
+    `個人で制作したPythonを使用した謎解き脱出ゲームです。
+    目が覚めると知らない家にいた主人公がどうにか脱出するゲームとなっています。`,
   ],
   picture1: [
     'project_img/ryuou_battle_1.png',
     'project_img/2dshoot_1.png',
     'project_img/kanji_1.png',
     'project_img/crl_1.png',
+    'project_img/fill_1.png',
+    'project_img/gohome_1.png',
   ],
   picture2: [
     'project_img/ryuou_battle_2.png',
     'project_img/2dshoot_2.png',
     'project_img/kanji_2.png',
     'project_img/crl_2.png',
+    'project_img/fill_2.png',
+    'project_img/gohome_2.png',
   ],
   picture3: [
     'project_img/ryuou_battle_3.png',
     'project_img/2dshoot_3.png',
     'project_img/kanji_3.png',
     'project_img/crl_3.png',
+    'project_img/fill_3.png',
+    'project_img/gohome_3.png',
   ],
   url: [
     //ryuou_battle
@@ -43,6 +55,10 @@ const caption = {
     'https://nato-git.github.io/kanji-dictionary/home',
     //CRL
     'https://github.com/nato-git/CharacterListApp.git',
+    //FillSupporter
+    'https://github.com/nato-git/FillSupporterBehavior.git',
+    //Gohome
+    'https://github.com/nato-git/EscapeGameByPython.git',
   ],
 };
 
